b0a67a0bd0dc93bb80779f86516662b809c34b8a
MingWai <mingwai@unity3d.com>
2020-11-15 19:00:27 +0100

    script

---
+++ Assets/Stereogram/RotateY.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateY : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void LateUpdate()
+    {
+        this.transform.RotateAround(Vector3.zero,Vector3.up,Time.deltaTime*speed);
+    }
+}
